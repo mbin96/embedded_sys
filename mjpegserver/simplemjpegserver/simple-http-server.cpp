3ba30b99e202f841c742341cbe85586ac5e6f49a
HY CHO <mbin96@gmail.com>
2019-12-09 01:27:21 +0900

    mjpeg comlite and make cpp class

--- mjpegserver/simplemjpegserver/simple-http-server.cpp
+++ mjpegserver/simplemjpegserver/simple-http-server.cpp
@@ -10,35 +10,48 @@
 #include <netdb.h>
 #include <netinet/in.h>
 #include <math.h>
+#include <signal.h>
+#include <iostream>
+#include <string>
+#include <fstream>
 //https://tjcplpllog.blogspot.com/2015/12/c-http.html
 //http://bbs.nicklib.com/application/1539
+//https://stackoverflow.com/questions/47729941/mjpeg-over-http-specification
 
-void int2Char(int src, char * out, int * stringSize){
-	int lenSrc = 0;
-    int i;
-	//길이재기
-    int cpIn = src;
-	for(i = 10; cpIn > 0;){
-        cpIn = (int)cpIn/i;
-		lenSrc++;
-	}
-	//동적할ㅇ당하기
-	out = (char *) malloc((lenSrc+1)*sizeof(char));
-	int j;
-	//ascii로 변환
-	for(j = 0; lenSrc > j; j++ ){
-		out[j] = (src/(int)pow(10, lenSrc-1-j))+48;
-		src = src%((int)pow(10, lenSrc-1-j));
-	}
-	*stringSize = lenSrc;
-	out[lenSrc] = '\0';
-	return;
-}
+// void int2Char(int src, char * out, int * stringSize){
+// 	int lenSrc = 0;
+//     int i;
+// 	//길이재기
+//     int cpIn = src;
+// 	for(i = 10; cpIn > 0;){
+//         cpIn = (int)cpIn/i;
+// 		lenSrc++;
+// 	}
+// 	//동적할ㅇ당하기
+// 	out = (char *) malloc((lenSrc+1)*sizeof(char));
+// 	int j;
+// 	//ascii로 변환
+// 	for(j = 0; lenSrc > j; j++ ){
+// 		out[j] = (src/(int)pow(10, lenSrc-1-j))+48;
+// 		src = src%((int)pow(10, lenSrc-1-j));
+// 	}
+// 	*stringSize = lenSrc;
+// 	out[lenSrc] = '\0';
+// 	return;
+// }
 
-int server(){
-	
-	int sock;
+#define __THROW_SEND_ERROR 1
+#define __THROW_SOCKET_ERROR 2
+#define __THROW_SET_SOCK_OPT_ERROR 3
+#define __THROW_BIND_ERROR 4
+#define __THROW_LISTEN_ERROR 5
+#define __THROW_ACCEPT_ERROR 6
+#define DEBUG
+
+class mJpegServer{
+	private :
 	int client;
+	int sock;
 	struct sockaddr_in addr;
 	struct sockaddr_in client_addr;
 	socklen_t addr_len = sizeof(addr);
@@ -46,22 +59,96 @@ int server(){
 	int on = 1;
 	char buffer[1024] = {0,};
 	size_t read_len;
-	const char header_packet[] = "HTTP/1.0 200 OK\r\nServer: YourServerName\r\n Connection: close\r\nContent-Type:multipart/x-mixed-replace; boundary=myBoundary\r\n";
+	const char * header_packet = "HTTP/1.0 200 OK\r\nServer: EMPOSIII\r\n Connection: close\r\nContent-Type:multipart/x-mixed-replace; boundary=myBoundary\r\n\r\n";
+	const char * mjpegHeaderPacket = "--myBoundary\r\nContent-Type: image/jpeg\r\n\r\n";
+	char * jpegData;
+	int jpegSize;
+	std::ifstream fileJpeg;
 
-	//const char * header_packet_1 = "HTTP/1.0 200 OK\r\n Server: YourServerName\r\n Connection: close\r\n Content-Type: image/jpeg\r\nContent-length: ";
-	const char mjpegHeaderPacket[] = "--myBoundary\r\nContent-Type: image/jpeg\r\nContent-length: ";
-	//const char * jpegSize = "3656\r\n\r\n";
-	const char * end = "\r\n\r\n";
+	public:
+	void cpJpegData(char * Data, int Size);
+	void openJpegData(std::string fileName);
 
+	void initSocket(int port=8080);
+	void createConnetion();
+	void closeConnection();
+	void closeSocket();
+
+	void sendFrame(char * jpegData, int jpegSize);
+	void sendFrame();
+	
+};
+
+void mJpegServer::cpJpegData(char * Data, int Size){
+	memcpy(jpegData,Data,Size);
+	jpegSize = Size;
+}
+
+void mJpegServer::openJpegData(std::string fileName){
+	delete[] jpegData;
+	fileJpeg.close();
+
+
+	fileJpeg.open(fileName, std::ios::binary);
+	if (!fileJpeg.is_open()){
+		throw std::__throw_ios_failure;
+	}
+	
+	fileJpeg.seekg(0, std::ios::end);
+	jpegSize = fileJpeg.tellg();
+	fileJpeg.seekg(0, std::ios::beg);
+
+	jpegData = new char[jpegSize];
+	fileJpeg.read(jpegData,jpegSize);
+
+}
+
+void mJpegServer::createConnetion(){
+
+	client = accept(sock, (struct sockaddr*)&client_addr, &addr_len);
+	if (client < 0) {
+		perror("accept() error");
+		throw __THROW_ACCEPT_ERROR;
+	}
+	//응답 받아오기
+	read_len = recv(client, buffer, sizeof(buffer), 0);
+	
+	#ifdef DEBUG
+	if (read_len > 0) {
+		printf("RECV: %s", buffer);
+	}
+	#endif //DEBUG
+	//헤더 패킷
+	send(client, header_packet, strlen(header_packet), 0);
+}
+
+void mJpegServer::closeConnection(){
+	close(client);
+#ifdef DEBUG
+	printf("closeSocket\n");
+#endif //DEBUG
+
+}
+void mJpegServer::closeSocket(){
+	close(sock);
+#ifdef DEBUG
+	printf("closeSocket\n");
+#endif //DEBUG
+
+}
+
+void mJpegServer::initSocket(int port){
+	//매개변수로 포트 설정
+	bind_port = port;
 	sock = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP);
 	if (sock < 0) {
 		perror("socket() error");
-		return 1;
+	 	throw __THROW_SOCKET_ERROR;
 	}
 	
 	if (setsockopt(sock, SOL_SOCKET, SO_REUSEADDR, (void*)&on, sizeof(on)) != 0) {
 		perror("setsockopt() error");
-		return 1;
+		throw __THROW_SET_SOCK_OPT_ERROR;
 	}
 
 	addr.sin_family = AF_INET;
@@ -69,81 +156,36 @@ int server(){
 	addr.sin_port = htons(bind_port);
 	if (bind(sock, (struct sockaddr*)&addr, addr_len) != 0) {
 		perror("bind() error");
-		return 1;
+		throw __THROW_BIND_ERROR;
 	}
 
 	if (listen(sock, 5) != 0) {
 		perror("listen() error");
-		return 1;
+		throw __THROW_LISTEN_ERROR;
 	}
-
 	printf("listening... %d\n", bind_port);
+}
+		
+	
 
 
-	client = accept(sock, (struct sockaddr*)&client_addr, &addr_len);
-	if (client < 0) {
-		perror("accept() error");
-		return 1;
+void mJpegServer::sendFrame(){
+	int err;
+
+	err = send(client, mjpegHeaderPacket, strlen(mjpegHeaderPacket), MSG_NOSIGNAL);
+	if (err < 0){
+		throw __THROW_SEND_ERROR;
 	}
-	read_len = recv(client, buffer, sizeof(buffer), 0);
-	if (read_len > 0) {
-		printf("RECV: %s", buffer);
+
+	err = send(client, jpegData, jpegSize, MSG_NOSIGNAL);
+	if (err < 0){
+		throw __THROW_SEND_ERROR;
 	}
-		
-	//jpeg 데이터
-	
-		char fileName[256];
-		//char * fileSizeString;
-		int fileSizeStringSize;
-	int count = 1;
-	send(client, header_packet, strlen(header_packet), 0);
-	printf(header_packet);	
-	while(1){
-		sprintf(fileName, "%d.jpeg",count++);
-		if (count == 3){
-			count = 2;
-		}
-		FILE *fp = fopen(fileName,"rb"); 
-		fseek(fp, 0, SEEK_END); 
-		int size = ftell(fp);
-		char * data = (char *) malloc((size + 1)* sizeof(char)); 
-		fseek(fp, 0, SEEK_SET);
-		fread(data,size,1,fp);
-		//sprintf(fileSize,"%d",size);
-		//printf("%d", strlen(fileSize));
-		char * fileSizeString;
-		int2Char(size, fileSizeString, &fileSizeStringSize);
-		sleep(1);
-		send(client, mjpegHeaderPacket, strlen(mjpegHeaderPacket), 0);
-		printf(mjpegHeaderPacket);
-		//char * dataLen = int2Char(sizeof(data));
-		//printf("1\n");
-		//content length
-		//printf(int2Char(size));
-		send(client, fileSizeString, fileSizeStringSize, 0);
-		send(client, end ,strlen(end),0);
-		
-		printf("%s",fileSizeString);
-		printf(end);
-		//printf(int2Char(size));
-		//send(client, "\r\n\r\n",strlen("\r\n\r\n"),0);
-		//send(client, jpegSize,strlen(jpegSize),0);
-		send(client, data, size,0);
-		send(client, "\r\n", strlen("\r\n"),0);
-		//send(client, end ,strlen(end),0);
-		//printf(end);
-		// read_len = recv(client, buffer, sizeof(buffer), 0);
-		// if (read_len > 0) {
-		// 	printf("RECV: %s", buffer);
-	
-		// 	//send(client, "\r\n",strlen("\r\n"),0);
-		// 	//send(client, "\r\n\r\n",strlen("\r\n\r\n"),0);
-		// }
-		free(data);
-		//free(fileSizeString);
+
+	err = send(client, "\r\n", strlen("\r\n"), MSG_NOSIGNAL);
+	if (err < 0){
+		throw __THROW_SEND_ERROR;
 	}
-	close(client);
-	close(sock);
 	
 }
 
@@ -161,10 +203,33 @@ int main(int argc, char *args[]) {
 	// default : 
 	// 	return 0;
 	// }
-	// //chdir("/");
-	// //setsid();
+	//chdir("/");
+	//setsid();
+	mJpegServer server;
+	server.initSocket(8080);
+	server.createConnetion();
 
-	server();
+		int count = 1 ;
+	while(1){
+		usleep(30000);
+		server.openJpegData(std::to_string(count)+".jpeg");
+		count++;
+		if(count == 3){
+			count = 1;
+		}
+		try{
+			server.sendFrame();
+		}catch(int x){
+			server.closeConnection();
+#ifdef DEBUG
+			std::cout<<"connection cut off puri"<< std::endl;
+#endif //DEBUG
+			server.createConnetion();
+		}
+	}
+	server.closeSocket();
+	//정상적인 경우 여기로 오면 안됨.
+	printf("quit mjpeg server");
     return 0;
 }
 
